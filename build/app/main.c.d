89c7b1230c72530a7b137bd0c333dbe0a8de77c6
Sadia Afreen <sadiaafreen@u.boisestate.edu>
2024-11-07 00:29:38 -0700

    passes all 3 tests

---
+++ build/app/main.c.d
@@ -0,0 +1,2 @@
+build/app/main.c.o: app/main.c app/../src/lab.h
+app/../src/lab.h:
---
+++ build/tests/harness/unity.c.d
@@ -0,0 +1,4 @@
+build/tests/harness/unity.c.o: tests/harness/unity.c \
+ tests/harness/unity.h tests/harness/unity_internals.h
+tests/harness/unity.h:
+tests/harness/unity_internals.h:
